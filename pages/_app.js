f41ff604d601d7a16e3b16b127fb37ae0b44e581
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-10-29 23:51:07 +0700

    Add material UI config 🌻

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import React from 'react'
+import Head from 'next/head'
+import { wrapper } from '../store/store'
+import '../styles/tailwind.css'
+import theme from '../config/theme'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function WrappedApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>Hello Next</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
-export default MyApp
+export default wrapper.withRedux(WrappedApp)
